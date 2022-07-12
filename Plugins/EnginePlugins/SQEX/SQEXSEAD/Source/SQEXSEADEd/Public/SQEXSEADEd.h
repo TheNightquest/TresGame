618ea12780c74d9b8db9b9c8406bdf8e18428cde
Narknon <narknon0@gmail.com>
2022-07-12 10:37:24 -0400

    Merge branch 'TresGame-Built' into Dev-Main

--- Plugins/EnginePlugins/SQEX/SQEXSEAD/Source/SQEXSEADEd/Public/SQEXSEADEd.h
+++ Plugins/EnginePlugins/SQEX/SQEXSEAD/Source/SQEXSEADEd/Public/SQEXSEADEd.h
@@ -1,11 +1,15 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
 #pragma once
-#include "CoreMinimal.h"
-#include "UObject/Object.h"
 
-class USQEXSEADEd : public UObject
+#include "CoreMinimal.h"
+#include "ModuleManager.h"
+
+class FSQEXSEADEdModule : public IModuleInterface
 {
 public:
 
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
 };
--- Source/TresGame/Public/Camera/TresCameraManager.h
+++ Source/TresGame/Public/Camera/TresCameraManager.h
@@ -16,7 +16,7 @@ class ATresCameraBase;
 class UParticleSystemComponent;
 class UCameraShake;
 
-UCLASS(NonTransient)
+UCLASS(NonTransient, BlueprintType)
 class ATresCameraManager : public APlayerCameraManager {
     GENERATED_BODY()
 public:
@@ -69,7 +69,7 @@ protected:
     UPROPERTY(Transient, BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     AActor* m_DebugOrgCameraTarget;
     
-    UPROPERTY(Transien, BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
+    UPROPERTY(Transient, BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     ATresCameraNormal* m_DebugPatrolCamera;
     
 public:
